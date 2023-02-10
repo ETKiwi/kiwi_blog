f4d253e382a5524173a3034a42b6299c69d7fdb6
ETKiwi <billnd12765@gmail.com>
2023-02-10 10:38:15 +0800

    [new]matplot_chinese_garble.py

---
+++ matplot_chinese_garble.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 10 09:29:03 2023
+
+@author: user
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+# 建立資料
+def data():
+    x = np.random.randint(1, 50, 10) # 10 random data from 1 to 50
+    y = np.random.randint(-20, 0, 10) # 10 random data from 1 to 50
+    # y = np.random.randn(10) # standard normal distribution random data
+    
+    return x, y
+
+# 繪圖
+def plt_plot(x, y, title = '繪圖'):
+    plt_chinese
+    # plt.xkcd()
+    
+    plt.plot(
+        x, y, # data
+        'r', # color
+        alpha = 0.5 # 透明度
+        )
+    # 圖說明
+    plt.title(title)
+    plt.xlabel('x')
+    plt.ylabel('y')
+
+    plt.show()
+    
+    
+def plt_chinese():
+    plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei'] # 顯示中文
+    plt.rcParams['axes.unicode_minus'] = False # 顯示負號
+
+
+
+x,y  = data()
+plt_plot(x, y)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
